592f95dac7779e4e8da361a9be64bdd5d75ad6f7
machoboy <jiwon@vertigogames.com>
2019-04-23 18:38:08 -0700

    4.23.2019

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+const errorController = require("./controller/404");
 
 const app = express();
 
@@ -12,17 +12,15 @@ app.set("view engine", "ejs");
 //app.set('view engine', 'pug');
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
